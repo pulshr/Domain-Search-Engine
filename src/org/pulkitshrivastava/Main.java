eb766a618be598098eb5c2a8076845f09f4771bb
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-09-17 13:17:01 +0530

    Created DomainNameAndRankPair Class

--- src/org/pulkitshrivastava/Main.java
+++ src/org/pulkitshrivastava/Main.java
@@ -1,8 +1,15 @@
 package org.pulkitshrivastava;
 
 public class Main {
-
+    public static class DomainNameAndRankPair {
+        String domainName;
+        int domainRank;
+        DomainNameAndRankPair(String name, int rank) {
+            this.domainName = name;
+            this.domainRank = rank;
+        }
+    }
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
