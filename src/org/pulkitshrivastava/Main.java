e770bce9d97098222a2435758783127d7093c464
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-09-17 13:27:48 +0530

    Create TrieNode Class

--- src/org/pulkitshrivastava/Main.java
+++ src/org/pulkitshrivastava/Main.java
@@ -1,5 +1,7 @@
 package org.pulkitshrivastava;
 
+import java.util.ArrayList;
+
 public class Main {
     public static class DomainNameAndRankPair {
         String domainName;
@@ -7,6 +9,14 @@ public class Main {
         DomainNameAndRankPair(String name, int rank) {
             this.domainName = name;
             this.domainRank = rank;
+        }
+    }
+    public static class TrieNode {
+        TrieNode[] children;
+        ArrayList<Integer> domainIndexes;
+        TrieNode() {
+            this.children = new TrieNode[26];
+            this.domainIndexes = new ArrayList<>();
         }
     }
     public static void main(String[] args) {
